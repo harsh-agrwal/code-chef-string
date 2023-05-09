d54fe92dcdb7e832cfe702104fad687d984bb4ae
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-05-09 08:03:08 +0530

    Create Pawri Meme.java

---
+++ Pawri Meme.java
@@ -0,0 +1,20 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0){
+		    String str = sc.next();
+		    System.out.println(str.replaceAll("party", "pawri"));
+		    T--;
+		}
+	}
+}
