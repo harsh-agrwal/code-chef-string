381e4d9469dd9ba155d26bb249f2c8df10dbe3dd
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-06-02 21:49:01 +0530

    Create Favourite String of Chef.java

---
+++ Favourite String of Chef.java
@@ -0,0 +1,78 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+ static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+  
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+  
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+  
+        int nextInt() { return Integer.parseInt(next()); }
+  
+        long nextLong() { return Long.parseLong(next()); }
+  
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+  
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    public static void main (String[] args) {
+        /* code */
+    
+    Scanner ss=new Scanner(System.in);
+    int t=ss.nextInt();
+    while(t-->0){
+        int n=ss.nextInt();
+        String str=ss.next();
+        String ans="";
+        int st=str.indexOf("code");
+        int ed=str.indexOf("chef");
+        if(st<ed&&ed!=-1){
+            ans+="AC";
+        }else{
+            ans+="WA";
+        }
+        System.out.println(ans);
+    }
+    }
+    }
