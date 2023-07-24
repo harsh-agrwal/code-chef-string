cbc056ef87aa1c5a5a823308a599f547161c3e04
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-07-24 09:44:20 +0530

    Create Chef and Meetings.java

---
+++ Chef and Meetings.java
@@ -0,0 +1,65 @@
+/* package codechef; // don't place package name! */
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        for (int tc = 0; tc < T; ++tc) {
+            String P = readTime(sc);
+            int N = sc.nextInt();
+            String[] L = new String[N];
+            String[] R = new String[N];
+            for (int i = 0; i < N; ++i) {
+                L[i] = readTime(sc);
+                R[i] = readTime(sc);
+            }
+
+            System.out.println(solve(P, L, R));
+        }
+
+        sc.close();
+    }
+
+    static String readTime(Scanner sc) {
+        String part1 = sc.next();
+        String part2 = sc.next();
+
+        return String.format("%s %s", part1, part2);
+    }
+
+    static String solve(String P, String[] L, String[] R) {
+        int pMinutes = convertToMinutes(P);
+
+        return IntStream.range(0, L.length)
+                .mapToObj(
+                        i ->
+                                (convertToMinutes(L[i]) <= pMinutes && convertToMinutes(R[i]) >= pMinutes)
+                                        ? "1"
+                                        : "0")
+                .collect(Collectors.joining());
+    }
+
+    static int convertToMinutes(String time) {
+        int hour = Integer.parseInt(time.substring(0, 2));
+        int minute = Integer.parseInt(time.substring(3, 5));
+        String half = time.substring(6);
+
+        if (hour == 12 && half.equals("AM")) {
+            hour = 0;
+        } else if (hour != 12 && half.equals("PM")) {
+            hour += 12;
+        }
+
+        return hour * 60 + minute;
+
+    }
+
+}
